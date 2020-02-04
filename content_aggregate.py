60edfe5d2d626cff24ed4a9ed2807ecc7731862c
VighneshPath <pathrikarvighnesh@gmail.com>
2020-02-04 12:40:11 +0530

    Added .strip for scraping data and modified amazon.in search

--- content_aggregate.py
+++ content_aggregate.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 comp_parts_sites = {
     "%20":[("vedantcomputers","https://www.vedantcomputers.com/index.php?route=product/search&search=")],
@@ -7,7 +8,7 @@ comp_parts_sites = {
 }
 
 headers ={
-'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
+'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
 }
 
 class Part():
@@ -20,7 +21,10 @@ class Part():
         self.site = site
 
     def get_price(self):
-        return float(''.join(self.price[1:].split(",")))
+        try:
+            return float(''.join(self.price[1:].split(",")))
+        except:
+            return 0
 
     def __str__(self):
         return (self.title, self.price, self.site)
@@ -77,9 +81,9 @@ def mdcomp(soup, part_name):
     part_list = []
     for item in results:
         try:
-            title = item.h4.a.get_text()
-            price = item.find("span",{"class":"price-new"}).get_text()
-            link = item.h4.a['href']
+            title = item.h4.a.get_text().strip()
+            price = item.find("span",{"class":"price-new"}).get_text().strip()
+            link = item.h4.a['href'].strip()
             flag = 0
             for word in part_name.split(" "):
                 if(word not in title.lower()):
@@ -97,13 +101,14 @@ def amazon(soup, part_name):
     It returns a list of Part objects satisfying the part name
     It takes Arguments BeautifulSoup object of amazon.in part search, part name
     '''
+    soup = BeautifulSoup(soup.prettify(),"html.parser")
     results = soup.findAll("div", {"class":"a-section a-spacing-medium"})
     part_list = []
     for item in results:
         try:
-            title = item.find("span",{"class":"a-size-medium a-color-base a-text-normal"}).get_text()
-            price = item.find("span",{"class":"a-offscreen"}).get_text()
-            link ="https://amazon.in"+item.find("a",{"class":"a-link-normal a-text-normal"})['href']
+            title = item.find("span",{"class":"a-size-medium a-color-base a-text-normal"}).get_text().strip()
+            price = item.find("span",{"class":"a-offscreen"}).get_text().strip()
+            link ="https://amazon.in"+item.find("a",{"class":"a-link-normal a-text-normal"})['href'].strip()
             flag = 0
             for word in part_name.split(" "):
                 if(word not in title.lower()):
@@ -125,9 +130,9 @@ def vedcomp(soup, part_name):
     part_list = []
     for item in results:
         try:
-            title = item.find("h4",{"class":"name"}).get_text()
+            title = item.find("h4",{"class":"name"}).get_text().strip()
             if(item.find("span",{"class":"price-new"}) == None):
-                price = item.find("p",{"class":"price"}).get_text().strip()
+                price = item.find("p",{"class":"price"}).get_text().strip().strip()
             else:
                 price = item.find("span",{"class":"price-new"}).get_text().strip()
             link = item.find("h4",{"class":"name"}).a['href']
@@ -152,15 +157,15 @@ def primeabgb(soup, part_name):
     part_list = []
     for item in results:
         try:
-            title = item.h3.a.get_text()
+            title = item.h3.a.get_text().strip()
             try:
                 try:
-                    price = item.findAll("span",{"class":"woocommerce-Price-amount amount"})[1].get_text()
+                    price = item.findAll("span",{"class":"woocommerce-Price-amount amount"})[1].get_text().strip()
                 except:
-                    price = item.find("span",{"class":"woocommerce-Price-amount amount"}).get_text()
+                    price = item.find("span",{"class":"woocommerce-Price-amount amount"}).get_text().strip()
             except:
                 continue
-            link = item.h3.a['href']
+            link = item.h3.a['href'].strip()
             flag = 0
             for word in part_name.split(" "):
                 if(word not in title.lower()):
@@ -208,7 +213,8 @@ def sort_according_to_price(part_list):
 if __name__ == '__main__':
     print("Enter Part Name")
     part_name = input()
+    part_name = part_name.lower()
     part_list = get_part(part_name)
     part_list = sort_according_to_price(part_list)
     for part in part_list:
-        print(part.title, part.price, part.site)
+        print(part.title, part.price, part.site[:40])
