2e0fd91c7e179e322d31580387775f50dea29e89
VighneshPath <pathrikarvighnesh@gmail.com>
2020-02-07 07:14:34 +0530

    Added Timestamp and Better O/P Formatting

--- content_aggregate.py
+++ content_aggregate.py
@@ -228,8 +228,14 @@ def sort_according_to_price(part_list):
 if __name__ == '__main__':
     print("Enter Part Name")
     part_name = input()
+    start_time = time.time()
     part_name = part_name.lower()
     part_list = get_part(part_name)
     part_list = sort_according_to_price(part_list)
+    if(part_list == []):
+        print("No Part Name",part_name,"Found")
     for part in part_list:
+        print("\n\n")
         print(part.title, part.price, part.site)
+    print("\n\n")
+    print("Time Taken", time.time()-start_time)
