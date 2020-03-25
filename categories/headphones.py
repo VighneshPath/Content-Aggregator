5b4721efce8dae528839b491c8d08b8ad289deea
ANUSHREE SDIXIT <krishnankitdixit@gmail.com>
2020-03-25 13:46:32 +0530

    headphones.py file

--- categories/headphones.py
+++ categories/headphones.py
@@ -1,5 +1,19 @@
-def scrape_site(site, part_name, soup):
+from bs4 import BeautifulSoup
+import requests
 
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0"}
+
+part_sites ={"%20":[("flipkart" , "https://www.flipkart.com/search?q=headphones&sid=0pm%2Cfcn&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_0_10_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_0_10_na_na_na&as-pos=0&as-type=HISTORY&suggestionId=headphones%7CHeadphones+%26+Earphones&requestId=05654bd0-e28d-4421-a42f-328d90a25cd2&sort=popularity")] , 
+"+":[("headphonezone" , "https://www.headphonezone.in/collections/bluetooth-wireless-earphones") , 
+      ("snapdeal" , "https://www.snapdeal.com/search?keyword=headphones&santizedKeyword=&catId=&categoryId=0&suggested=false&vertical=&noOfResults=20&searchState=&clickSrc=go_header&lastKeyword=&prodCatId=&changeBackToAll=false&foundInAll=false&categoryIdSearched=&cityPageUrl=&categoryUrl=&url=&utmContent=&dealDetail=&sort=rlvncy") , 
+      ("ebay" , "https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=headphones&_sacat=0")]
+} 	
+
+
+
+
+def scrape_site(site, part_name, soup):
+    
     if(site == "headphonezone"):
         site = headphonezone
     elif(site == "flipkart"):
@@ -44,7 +58,7 @@ def heaphonezone(soup , part_name):
 					break
 				if(flag == 0):
 					part_list.append((title,price,link))
-
+							
 
 		except:
 			continue
@@ -70,22 +84,19 @@ def flipkart(soup , part_name):
 
 			link_ = result.find("a" , {"class":"Zhf2z-"})
 			link = link_["href"]
-
+			
 			# img = result.find()
 
 			flag = 0
 
 			for word in part_name.split(" "):
 				if(word not in title.lower().split()):
-					flag = 1
+                	flag = 1
 					break
 				if(flag == 0):
 					part_list.append((title,price,link))
-
-
 		except:
 			continue
-
 	return part_list
 
 # # SANPDEAL
@@ -144,7 +155,7 @@ def ebay(soup , part_name):
 			price = result.find("div" , {"class":"s-item__detail s-item__detail--primary"}).span.get_text().strip()
 
 			link = result.find("a" , {"class":"s-item__link"})["href"]
-
+			
 			flag = 0
 
 			for word in part_name.split(" "):
