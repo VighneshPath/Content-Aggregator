976ec704b0888554889e13c013aa98a4ec743d91
rizwan-sayyed <60218766+rizwan-sayyed@users.noreply.github.com>
2020-04-15 13:08:46 +0530

    shoes.py file

--- categories/shoes.py
+++ categories/shoes.py
@@ -4,89 +4,73 @@
 # In[1]:
 
 
-import requests              
-from bs4 import BeautifulSoup as bs
-res=requests.get("https://www.libertyshoesonline.com/")
-
-
-soup=bs(res.text,"html.parser")
-result=soup.findAll("div",{"class":"info-box"})
-for i in result:
-    try:
-        title=i.a.h3.getText()
-        print(title)
-        price=i.p.span.getText()
-        print(price)
-    except:
-        continue
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-
-
-# In[39]:
-
-
 import requests
-from bs4 import BeautifulSoup as bs
-res=requests.get("https://www.flipkart.com/")
-soup=bs(res.text,"html.parser")
-result=soup.findAll("div",{"class":"_2kSfQ4"})
-for i in result:
-    try:
-        title=i.a.getText()
-        print(title)
-        #price=i.find("div",{"class":"BXlZdc"})
-        #print(price)
-    except:
-        continue
+from bs4 import BeautifulSoup
 
 
-# In[48]:
+part_sites={
+    "+":[("libertyshoes","https://www.libertyshoesonline.com/liberty/st-search?q=shoes=item goes here"),
+         ("buyhatke","https://compare.buyhatke.com/pricelist/big-fox-casual-shoes-price-in-india-hatke335504=item goes here")]
+}
 
 
-import requests
-from bs4 import BeautifulSoup as bs
-res=requests.get("https://compare.buyhatke.com/pricelist/big-fox-casual-shoes-price-in-india-hatke335504")
-soup=bs(res.text,"html.parser")
-result=soup.findAll("div",{"class":"padding-horizontal-1x"})
-for i in result:
-    try:
-        title=i.h3.getText()
-        print(title)
-         #price=i.find("div",{"class":"product-price u-textLeft padding-horizontal-1x"}).span.b.getText()
-         #print(price)
-    except:
-        continue
+def scrape_site(site, part_name, soup):
+    if(site == "libertyshoes"):
+        site=libertyshoes
+    elif(site == "buyhatke"):
+        site=buyhatke
+    part_list=site(soup, part_name)
+    return part_list
 
 
-# In[6]:
+def libertyshoes(soup, part_name):
+    results=soup.findAll("div",{"class":"best-box"})
+    part_list=[]
+    for item in results:
+        try:
+            title=item.find("div",{"class":"info-box"}).a.h3.getText().strip()
+            #print(title)
+            price=item.find("div",{"class":"info-box"}).p.span.getText().strip()
+            #print(price)
+            link=item.find("div",{"class":"info-box"}).a['href'].strip()
+            #print(link)
+            #img_link=item.a.img['data-src'].strip()
+           # print(img_link)
+            for value in part_name:
+                if(value not in title):
+                    flag=1
+                    break
+            if(flag==0):
+                part_list.append((title,price,link))
+        except:
+            continue
+    return part_list
 
 
-import requests
-from bs4 import BeautifulSoup as bs
-res=requests.get("https://www.amazon.in/s?k=shoes&ref=nb_sb_noss_2") 
-soup=bs(res.text,"html.parser")
-result=soup.findAll("h2",{"class":"a-size-mini a-spacing-none a-color-base s-line-clamp-2"})
-for i in result:
-    try:
-        title=i.a.getText()
-        print(title)
-    except:
-        continue
+def buyhatke(soup,part_name):
+    results=soup.findAll("div",{"class":"results-product product"})
+    for item in results:
+        try:
+            title=item.find("div",{"class":"padding-horizontal-1x"}).h3.getText().strip()
+            #print(title)
+            price=item.find("span",{"class":"product-price--value"}).b.getText().strip()
+           # print("Rs."+price)
+            link='https://compare.buyhatke.com/'+item.a['href'].strip()
+            #print('https://compare.buyhatke.com'+link)
+           # img_link=item.find("div",{"class":"product-img--wrap"}).img['data-original'].strip()
+            #print(img)
+            for value in part_name:
+                if(value not in title):
+                    flag=1
+                    break
+            if(flag==0):
+                part_list.append((title,price,link))
+        except:
+            continue            
+    return part_list
 
 
-# In[ ]:
+
 
 
 
