937192e21a8cb68289635ef160b5eb4d25aedff0
rizwan-sayyed <60218766+rizwan-sayyed@users.noreply.github.com>
2020-04-15 14:07:14 +0530

    shoes.py file

--- categories/shoes.py
+++ categories/shoes.py
@@ -8,63 +8,58 @@ import requests
 from bs4 import BeautifulSoup
 
 
-part_sites={
-    "+":[("libertyshoes","https://www.libertyshoesonline.com/liberty/st-search?q=shoes=item goes here"),
-         ("buyhatke","https://compare.buyhatke.com/pricelist/big-fox-casual-shoes-price-in-india-hatke335504=item goes here")]
+parts_sites={
+    "+":[("www.libertyshoesonline.com","https://www.libertyshoesonline.com/liberty/st-search?q=shoes=item goes here"),
+         ("www.compare.buyhatke.com","https://compare.buyhatke.com/pricelist/big-fox-casual-shoes-price-in-india-hatke335504=item goes here")]
 }
 
 
 def scrape_site(site, part_name, soup):
-    if(site == "libertyshoes"):
-        site=libertyshoes
-    elif(site == "buyhatke"):
-        site=buyhatke
-    part_list=site(soup, part_name)
+    if(site == "www.libertyshoesonline.com"):
+        site_function=libertyshoes
+    elif(site == "www.compare.buyhatke.com"):
+        site_function=buyhatke
+    part_list=site_function(soup, part_name,site)
     return part_list
 
 
-def libertyshoes(soup, part_name):
+def libertyshoes(soup, part_name,site):
     results=soup.findAll("div",{"class":"best-box"})
     part_list=[]
     for item in results:
         try:
             title=item.find("div",{"class":"info-box"}).a.h3.getText().strip()
-            #print(title)
             price=item.find("div",{"class":"info-box"}).p.span.getText().strip()
-            #print(price)
             link=item.find("div",{"class":"info-box"}).a['href'].strip()
-            #print(link)
-            #img_link=item.a.img['data-src'].strip()
-           # print(img_link)
-            for value in part_name:
-                if(value not in title):
+            img_link=item.a.img['data-src']
+            flag=0
+            for value in part_name.split(" "):
+                if(value not in title.lower().split()):
                     flag=1
                     break
             if(flag==0):
-                part_list.append((title,price,link))
+                part_list.append((title,price,link,img_link,site))
         except:
             continue
     return part_list
 
 
-def buyhatke(soup,part_name):
+def buyhatke(soup,part_name,site):
     results=soup.findAll("div",{"class":"results-product product"})
+    part_list=[]
     for item in results:
         try:
             title=item.find("div",{"class":"padding-horizontal-1x"}).h3.getText().strip()
-            #print(title)
-            price=item.find("span",{"class":"product-price--value"}).b.getText().strip()
-           # print("Rs."+price)
+            price="Rs."+item.find("span",{"class":"product-price--value"}).b.getText().strip()
             link='https://compare.buyhatke.com/'+item.a['href'].strip()
-            #print('https://compare.buyhatke.com'+link)
-           # img_link=item.find("div",{"class":"product-img--wrap"}).img['data-original'].strip()
-            #print(img)
-            for value in part_name:
-                if(value not in title):
+            img_link=item.find("div",{"class":"product-img--wrap"}).img['data-original']
+            flag=0)
+            for value in part_name.split(" "):
+                if(value not in title.lower().split()):
                     flag=1
                     break
             if(flag==0):
-                part_list.append((title,price,link))
+                part_list.append((title,price,link,img_link,site))
         except:
             continue            
     return part_list
