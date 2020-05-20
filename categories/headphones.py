695fad3619af02ecf72e0bf04572fd6b2669e0be
ANUSHREE SDIXIT <krishnankitdixit@gmail.com>
2020-05-20 20:05:52 +0530

    Headphones.py modified

--- categories/headphones.py
+++ categories/headphones.py
@@ -3,7 +3,7 @@ import requests
 
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0"}
 
-parts_sites ={"%20":[("www.flipkart.com" , "https://www.flipkart.com/search?q=item goes here")] , "+":[("www.snapdeal.com" , "https://www.snapdeal.com/search?keyword=item goes here")] , "-":[("www.headphonezone.in" , "https://www.headphonezone.in/collections/item goes here")]
+parts_sites ={"%20":[("www.flipkart.com" , "https://www.flipkart.com/search?q=item goes here")] , "+":[("www.snapdeal.com" , "https://www.snapdeal.com/search?keyword=item goes here"),("www.amazon.in", "https://www.amazon.in/s?k=item goes here")] , "-":[("www.headphonezone.in" , "https://www.headphonezone.in/collections/item goes here")]
 }
 
 
@@ -13,8 +13,8 @@ parts_sites ={"%20":[("www.flipkart.com" , "https://www.flipkart.com/search?q=it
 
 def scrape_site(site, part_name, soup):
 
-    if(site == "www.headphonezone.in"):
-        site_function = headphonezone
+    if(site == "www.amazon.in"):
+        site_function = amazon
     elif(site == "www.flipkart.com"):
         site_function = flipkart
     elif(site == "www.snapdeal.com"):
@@ -31,22 +31,53 @@ def scrape_site(site, part_name, soup):
 
 # soup = BeautifulSoup(html.text , "html.parser")
 
-def headphonezone(soup , part_name , site):
-    part_list = []
-    results = soup.find_all("div" , {"class":"product-wrap"})
-    for item in results:
-        try:
-            result = item.find("div" , {"class":"product-details"})
+# def headphonezone(soup , part_name , site):
+#     part_list = []
+#     results = soup.find_all("div" , {"class":"product-wrap"})
+#     for item in results:
+#         try:
+#             result = item.find("div" , {"class":"product-details"})
 
-            link = item.a["href"]
+#             link = item.a["href"]
 
-            title = result.find("span" , {"class":"title"}).get_text().strip()
+#             title = result.find("span" , {"class":"title"}).get_text().strip()
 
-			price = result.find("span" , {"class":"money"}).get_text().strip().replace("₹","Rs.")
+# 			price = result.find("span" , {"class":"money"}).get_text().strip().replace("Rs.","₹")
 
-            img = item.div.a["href"]
+#             img = item.div.a["href"]
 
-            flag = 0
+#             flag = 0
+
+#             for word in part_name.split(" "):
+#                 if(word not in title.lower().split()):
+#                     flag = 1
+#                     break
+#             if(flag == 0):
+#                     part_list.append((title,price,link,img,site))
+
+
+#         except:
+#             continue
+
+#     return part_list
+
+
+
+def amazon(soup , part_name , site):
+	part_list = []
+	results = soup.findAll("div", {"class": "a-section a-spacing-medium"})
+	for result in results:
+		try:
+			title = result.find("span" , {"class":"a-size-medium a-color-base a-text-normal"}).get_text().strip()
+
+			price = result.find("span", {"class": "a-offscreen"}).get_text().strip().replace("Rs.", "₹")
+
+			link = "https://amazon.in" + \
+				result.find("a", {"class": "a-link-normal a-text-normal"})['href'].strip()
+
+			img = result.find("div", {"class": "a-section aok-relative s-image-fixed-height"}).img["src"]
+
+			flag = 0
 
             for word in part_name.split(" "):
                 if(word not in title.lower().split()):
@@ -60,6 +91,10 @@ def headphonezone(soup , part_name , site):
             continue
 
     return part_list
+
+
+
+
 
 
 
@@ -82,7 +117,7 @@ def flipkart(soup , part_name , site):
         try:
             title = result.find("a" , {"class":"_2cLu-l"}).get_text().strip()
 
-            price = result.find("div" , {"class":"_1vC4OE"}).get_text().strip()replace("₹","Rs.")
+            price = result.find("div" , {"class":"_1vC4OE"}).get_text().strip()replace("Rs.","₹")
 
             link_ = result.find("a" , {"class":"Zhf2z-"})
             link = link_["href"]
@@ -155,7 +190,7 @@ def snapdeal(soup , part_name , site):
 
             title = result.a.p.get_text().strip()
 
-            price = result.find("span" , {"class":"lfloat product-price"}).get_text().strip()replace("₹","Rs.")
+            price = result.find("span" , {"class":"lfloat product-price"}).get_text().strip()replace("Rs.","₹")
 
             img_ = item.find("img" , {"class":"product-image"})
             img = img_["src"]
