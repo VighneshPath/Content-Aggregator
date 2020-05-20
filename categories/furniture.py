4b90568dde49c4b8ff8541c258bc577ea890f85a
VighneshPath <pathrikarvighnesh@gmail.com>
2020-05-20 18:34:32 +0530

    Fixed pepperfry

--- categories/furniture.py
+++ categories/furniture.py
@@ -3,83 +3,70 @@
 
 
 parts_sites = {
-     "+":[("www.amazon.in","https://www.amazon.in/s?k=item goes here")],
-     "%20":[("www.flipkart.com","https://www.flipkart.com/search?q=item goes here")],
-     "+":[("www.pepperfry.com","https://www.pepperfry.com/search?q=item goes here")]
+    "+": [("www.amazon.in", "https://www.amazon.in/s?k=item goes here"), ("www.pepperfry.com", "https://www.pepperfry.com/site_product/search?q=item goes here")]
 }
+
 
 def scrape_site(site, part_name, soup):
     if(site == "www.amazon.in"):
         site_function = amazon
-    elif(site == "www.flipkart.com"):
-        site_function = flipkart
     elif(site == "www.pepperfry.com"):
+        # print("PPF")
         site_function = pepperfry
-    part_list = site_function(soup,part_name,site)
+    part_list = site_function(soup, part_name, site)
     return part_list
 
-def amazon(soup,part_name,site):
-    results = soup.findAll("div",{"class":"a-section a-spacing-medium"})
-    part_list=[]
-    for item in results:
-        try:
-            title = item.find("span", {"class": "a-size-base-plus a-color-base a-text-normal"}).get_text().strip()
-            price = item.find("span", {"class": "a-price-whole"}).get_text().strip()
-            link = "https://amazon.in" + item.find("a", {"class": "a-link-normal a-text-normal"})['href'].strip()
-            img_link = item.find("img",{"class":"s-image"})['src']
-            f=0
-            for product in part_name.split(" "):
-                if(product not in title.lower.split()):
-                    f=1
-                    break
-            if(f==0):
-                part_list.append((title,price,link,img_link,site))   
-        except:
-            continue
-    return  part_list               
 
-def flipkart(soup,part_name,site):
-    results = soup.findAll("div",{"class":"bhgxx2 col-12-12"})
-    part_list=[]
+def amazon(soup, part_name, site):
+    results = soup.findAll("div", {"class": "a-section a-spacing-medium"})
+    part_list = []
     for item in results:
         try:
-            title=item.find("a",{"class":"_2cLu-l"}).get_text().strip()
-            price=item.find("div",{"class":"_1vC4OE"}).get_text().strip()
-            link = "https://flipkart.com"+ item.find("a",{"class":"_2cLu-l"})['href'].strip()
-            img_link = item.find("div",{"class":"_3BTv9X _3MSCRn"}).img['src']
-            f=0
+            title = item.find("span", {
+                              "class": "a-size-base-plus a-color-base a-text-normal"}).get_text().strip()
+            price = "₹" + item.find(
+                "span", {"class": "a-price-whole"}).get_text().strip()
+            link = "https://amazon.in" + \
+                item.find(
+                    "a", {"class": "a-link-normal a-text-normal"})['href'].strip()
+            img_link = item.find("img", {"class": "s-image"})['src']
+            # print(title, price, link, img_link)
+            f = 0
+            #print(title, part_name)
             for product in part_name.split(" "):
-                if(product not in title.lower.split()):
-                    f=1
+                if(product not in title.lower().split()):
+                    f = 1
                     break
-            if(f==0):
-              part_list.append((title,price,link,img_link,site))   
+            if(f == 0):
+                part_list.append((title, price, link, img_link, site))
         except:
             continue
     return part_list
 
-def pepperfry(soup,part_name,site):
-    results = soup.findAll("div",{"class":"pf-col srchrslt-crd-10x11 srch-rslt-cards pf-margin-bottom20 clipprods"})
-    part_list=[]
+
+def pepperfry(soup, part_name, site):
+    results = soup.findAll(
+        "div", {"class": "clip-crd-10x11 pf-white srch-rslt-bxwrpr"})
+    part_list = []
     for item in results:
         try:
-            title=(item.find("a",{"class":"clip-prd-dtl"}).text)
-            link="https://www.pepperfry.com/"+(item.find("a",{"class":"clip-prd-dtl"})['href'])
-            price = (item.find("div",{"class":"clip-price-blocks row"}).find("span",{"class":"clip-offr-price"}).text.strip().replace("Rs.","₹"))
-            img_link = item.find("div",{"class":"card-img-wrp center-xs card-srch-img-wrp"}).img['src']
-            f=0
+            title = item.find(
+                "div", {"class": "pf-col xs-12"}).h2.a.text.strip(" ")
+            link = item.find(
+                "div", {"class": "pf-col xs-12"}).h2.a["href"]
+
+            price = item.find(
+                "span", {"class": "clip-offr-price"}).text.strip().replace("Rs.", "₹")
+
+            img_link = item.find(
+                "div", {"class": "card-img-wrp center-xs card-srch-img-wrp"}).a.img["data-src"]
+            f = 0
             for product in part_name.split(" "):
-                if(product not in title.lower.split()):
-                    f=1
+                if(product not in title.lower().split()):
+                    f = 1
                     break
-            if(f==0):
-                part_list.append((title,price,link,img_link,site))
+            if(f == 0):
+                part_list.append((title, price, link, img_link, site))
         except:
             continue
-    return(part_list) 
-
-
-
-
-
-
+    return(part_list)
